c27ce609b4dcdab79516f4bd014f5b5d1825479d
abzieher <franz@abzieher.net>
2015-01-14 11:05:10 +0100

    Updated download url to the latest version

--- download-seafile.sh
+++ download-seafile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /opt/seafile
-curl -L -O https://bitbucket.org/haiwen/seafile/downloads/seafile-server_4.0.1_x86-64.tar.gz
+curl -L -O https://bitbucket.org/haiwen/seafile/downloads/seafile-server_4.0.4_x86-64.tar.gz
 tar xzf seafile-server_*
 mkdir -p installed
 mv seafile-server_* installed
